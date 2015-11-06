33aba83e0acb0ed7dffd2b58fe6e39a33a3a338c
John Phang <phangjohn1992@gmail.com>
2015-11-05 17:31:30 -0800

    testing how to use fork() wait pid and execv

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char * argv[])
 {
-	cout<<"Hello, World!"<<endl;
-	cout <<"Goodbye, World!"<<endl;
-	return 0;
+	char * args[2] = {"ls", NULL} ;
+	pid_t c_pid, pid;
+	int status;
+
+	c_pid = fork();
+
+	if(c_pid < 0)
+	{
+		perror("fork failed");
+		exit(1);
+	}
+
+	else if (c_pid == 0)
+	{
+		printf("child: executing ls\n");
+		execvp(args[0], args);
+		perror("execve failed");
+		
+	}
+	
+	else if (c_pid > 0)
+	{
+		if(pid = wait(&status) < 0)
+		{
+			perror("wait");
+			exit(1);
+		}
+
+		printf("Parent: Finished\n");
+	}
 
 }
